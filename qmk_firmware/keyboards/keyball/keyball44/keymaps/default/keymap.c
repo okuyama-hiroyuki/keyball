394490f5431bcf97819d16a0fa011954bafe6d4c
奥山 洋之 <131259894+okuyama-hiroyuki@users.noreply.github.com>
2024-12-29 19:53:00 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     KC_NO   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC   ,
-    KC_NO   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_ESC  , KC_ENT  ,
+    KC_NO   , KC_A     , KC_S     , LT(1, KC_D)     , LT(2, KC_F)     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_ESC  , KC_ENT  ,
     KC_NO   , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_NO  , KC_NO   , KC_NO  , KC_TAB  ,
               KC_NO,KC_NO,KC_LSFT     ,KC_LCTL,KC_LALT,                  OSL(1),KC_SPC, KC_NO,     KC_NO  , KC_NO
   ),
