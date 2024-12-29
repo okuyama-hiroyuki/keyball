801a07d1b15e418cbb5247ab34e58590c9aa001b
奥山 洋之 <131259894+okuyama-hiroyuki@users.noreply.github.com>
2024-12-29 18:58:12 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_universal(
     _______ ,  KC_1    , KC_2    , KC_3     , KC_4    , KC_5    ,                                         KC_6    , KC_7    , KC_8    , KC_9    , KC_0   , _______   ,
     _______ ,  KC_NO   , KC_UP   , KC_NO    , KC_NO  , KC_MINUS ,                                         KC_CIRC  , KC_AT  , KC_LBRC    , KC_RBRC  , KC_BSLS , _______   ,
-    _______ ,  KC_LEFT , KC_DOWN , KC_RIGHT , KC_NO , KC_SCOLON ,                                         KC_COLON  , KC_COMMA  , KC_DOT  , KC_SLSH  , KC_MINUS , _______  ,
+    _______ ,  KC_LEFT , KC_DOWN , KC_RIGHT , KC_NO , KC_SCLN   ,                                         KC_COLON  , KC_COMMA  , KC_DOT  , KC_SLSH  , KC_MINUS , _______  ,
                   _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
 
